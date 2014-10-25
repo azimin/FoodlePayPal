5462bc9cd32eb415b08b6b55eaf01383b2b5f1d9
Alex Zimin <azimin@me.com>
2014-10-25 13:40:28 +0400

    - Added base

---
+++ Foodle/Foodle/ViewControllers/Base/FLBaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FLBaseViewController.swift
+//  Foodle
+//
+//  Created by Alex Zimin on 25/10/14.
+//  Copyright (c) 2014 Foodlle. All rights reserved.
+//
+
+import UIKit
+
+class FLBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
