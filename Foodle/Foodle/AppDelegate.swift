c2292a8f0dc3c31a5d6e518cf77c54004667827f
Alex Zimin <azimin@me.com>
2014-10-26 09:57:32 +0300

    - Small fixes

--- Foodle/Foodle/AppDelegate.swift
+++ Foodle/Foodle/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var paymentsManager: FLPaymentsManager!
     var monitor: FLBeaconsMonitor!
     var kbroadCastBeacon = false
-    var isGuest = true
+    var isGuest = false
 
     class func sharedAppDelegate() -> AppDelegate {
         return UIApplication.sharedApplication().delegate as AppDelegate
